e77d21263fdeda0c3e6984807b5295fa8fc88053
Abhinav Saxena <abhinav150601@gmail.com>
2023-03-07 09:43:48 +0530

    Day 1

---
+++ Basic Programs/GoodTurn.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a;
+        cin >> b;
+        if(a+b>6) cout << "YES" << endl;
+        else cout << "NO" << endl; 
+    }
+    
+}
